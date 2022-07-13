c34faa730f213190975693a2fb8d9e767fe0fc03
monicaschneimercadolivre <monica.schneider@mercadolivre.com>
2022-07-13 15:22:57 -0300

    Conclusão da Arquitetura do desafio

--- src/main/java/com/desafio_quality/desafio_quality/controller/DistrictController.java
+++ src/main/java/com/desafio_quality/desafio_quality/controller/DistrictController.java
@@ -1,9 +1,16 @@
 package com.desafio_quality.desafio_quality.controller;
 
+import com.desafio_quality.desafio_quality.service.IDistrictService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/District")
 public class DistrictController {
+
+    @Autowired
+    private IDistrictService districtService;
+
+
 }
--- src/main/java/com/desafio_quality/desafio_quality/controller/RoomController.java
+++ src/main/java/com/desafio_quality/desafio_quality/controller/RoomController.java
@@ -1,10 +1,16 @@
 package com.desafio_quality.desafio_quality.controller;
 
 
+import com.desafio_quality.desafio_quality.service.IRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Room")
 public class RoomController {
+
+    @Autowired
+    private IRoomService roomService;
+
 }

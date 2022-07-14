86ab9e41277378ff9612dfbf70f43b2126fa279c
amandaamabili <amanda.amabili@mercadolivre.com>
2022-07-14 12:18:44 -0300

    Create past tests

---
+++ src/test/java/com/desafio_quality/desafio_quality/service/ResidenceServiceTest.java
@@ -0,0 +1,18 @@
+package com.desafio_quality.desafio_quality.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+
+public class ResidenceServiceTest {
+
+    @BeforeEach
+    public void setup(){
+
+    }
+}

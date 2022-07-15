0c475fd48078ee9462bd4196b8cd237b0c395edc
marinfarias <marina.farias@mercadolivre.com>
2022-07-15 20:27:06 -0300

    limpando os imports desnecessários

--- src/main/java/com/desafio_quality/desafio_quality/controller/DistrictController.java
+++ src/main/java/com/desafio_quality/desafio_quality/controller/DistrictController.java
@@ -2,10 +2,8 @@ package com.desafio_quality.desafio_quality.controller;
 
 import com.desafio_quality.desafio_quality.handler.HandlerException;
 import com.desafio_quality.desafio_quality.model.District;
-import com.desafio_quality.desafio_quality.model.Residence;
 import com.desafio_quality.desafio_quality.service.IDistrictService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
